c02baead8917ff81f899c0bdcab6da36dd9d5b0b
Alex Morton <am@alexmortoncreative.com>
2020-10-20 17:19:48 +0200

    up to date changes

--- src/components/PodcastView.js
+++ src/components/PodcastView.js
@@ -1,42 +1,57 @@
-import React, { useState } from 'react';
-import './View.css';
+import React from "react";
+import "./View.css";
 
-export default function PodcastView() {
-  const [episodes, setEpisodes] = ([]);
+export default class PodcastView extends React.Component {
+  state = {
+    episodes: []
+  };
 
-  fetch(`https://www.buzzsprout.com/api/1184462?api_token=${process.env.REACT_APP_ACCESS_KEY}`)
-    .then(res => res.json())
-    .then(json => {
-      console.log(json);
-    })
+  componentDidMount() {
+    fetch(
+      `https://www.buzzsprout.com/api/1184462/episodes.json?api_token=${process.env.REACT_APP_ACCESS_KEY}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        json.forEach((el) => this.setState({
+          episodes: [...this.state.episodes,
+          el]
+        }));
+        console.log(this.state.episodes);
+      });
+  }
 
-
-  return (
-    <div className='view-section'>
-      <h1 className='page-title'>The Ladies Code Collective Podcast</h1>
-      <EpisodeContainer />
-    </div>
-  )
+  render() {
+    return (
+      <div className="view-section">
+        <h1 className="page-title">The Ladies Code Collective Podcast</h1>
+        <EpisodeContainer episodes={this.state.episodes} />
+      </div>
+    );
+  }
 }
 
-
-function EpisodeContainer() {
+function EpisodeContainer({ episodes }) {
   return (
-    <div className='episode-container'>
-      <Episode />
-      <Episode />
-      <Episode />
-      <Episode />
+    <div className="episode-container">
+      <ul>
+        {
+          episodes.map(episode => (
+            <li key={episode.id}><Episode episode={episode} /></li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+}
+
+function Episode({ episode }) {
+  return (
+    <div className="episode">
+      <h2 className="episode-title">{episode.title}</h2>
       
     </div>
-  )
+  );
 }
 
-function Episode() {
-  return (
-    <div className='episode'>
-      <h2 className='episode-title'>Episode Title</h2>
-      <p className='episode-description'>Episode Description</p>
-    </div>
-  )
-}
+/* <div id="buzzsprout-player-5889634"></div>
+<script src="https://www.buzzsprout.com/1184462/5889634-lcc-14-shiny-object-syndrome-when-learning-to-code.js?container_id=buzzsprout-player-5889634&player=small" type="text/javascript" charset="utf-8"></script> */
